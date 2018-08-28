dd7b617da7c56810c40b572f7a583cd3ae0de886
Gabester0 <gceipper@gmail.com>
2018-08-27 23:33:16 -0400

    l120

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+
+import classes from './BuildControls.css';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Meat', type: 'meat'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Bacon', type: 'bacon'},
+    
+]
 
 const buildControls = (props) => (
-    <div>
-        <p>Doob</p>
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl=>(
+            <BuildControl key={ctrl.label} label={ctrl.label}/>
+        ))}
     </div>
 );
 
-export defualt buildControls;
+export default buildControls;
