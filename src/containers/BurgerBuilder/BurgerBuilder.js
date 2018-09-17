39abd3ce5b12cb3caecb52b7bd3633686dc49b51
Gabester0 <gceipper@gmail.com>
2018-09-16 22:59:14 -0400

    l30 and l131

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,11 +90,12 @@ purchaseContinueHandler = () =>{
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler} >
                     <OrderSummary 
                     ingredients={this.state.ingredients}
+                    price={this.state.totalPrice}
                     purchaseCancelled = {this.purchaseCancelHandler}
                     purchaseContinued = {this.purchaseContinueHandler} />    
                 </Modal> 
 
-                <Burger ingredients={this.state.ingredients}/>
+                <Burger ingredients={this.state.ingredients}/>e
                 <BuildControls
                 ingredientAdded={this.addIngredientHandler}  
                 ingredientRemoved={this.removeIngredientHandler}   
