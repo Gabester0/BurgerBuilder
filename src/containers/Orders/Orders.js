b01e947760d76fd514c2a791bb4095276b81ade6
Gabester0 <gceipper@gmail.com>
2018-12-07 18:37:44 -0500

    Lessons 222 and 223; Router & BurgerBuilder Wrapup

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -28,6 +28,12 @@ class Orders extends Component {
     render(){
         return(
             <div>
+                {this.state.orders.map(order =>(
+                    <Order key={order.id} 
+                    ingredients={order.ingredients}
+                    price={+order.price}/>
+                )
+                )}
                 <Order />
                 <Order />
             </div>
