4ad7867718ab722b4b1b99dd5e6eb31a88e6519e
Gabester0 <gceipper@gmail.com>
2018-12-06 23:16:34 -0500

    Plugging Along, finally cracked 50% progress

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,33 +83,12 @@ purchaseCancelHandler = () => {
 }
 
 purchaseContinueHandler = () =>{
-    // this.setState( { loading: true } );
-    // const order = {
-    //     ingredients: this.state.ingredients,
-    //     price: this.state.totalPrice,
-    //     customer: {
-    //         name: "Gabe Eipper",
-    //         address: {
-    //             street: 'testStreet Suite',
-    //             country: 'US',
-    //             state: 'PA',
-    //             zip: 17011
-    //         },
-    //         email: 'test@test.com',
-    //     },
-    //     delivery: 'fastest'
-    // }
-    // axios.post('/orders.json', order)
-    // .then(response => { this.setState( { loading: false , purchasing: false} );
-    // } )
-    // .catch(err => {
-    //     console.log(err);
-    //     this.setState( { loading: false , purchasing: false} );
-    // });
+
     const queryParams = [];
     for(let i in this.state.ingredients){
         queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]))
     };
+    queryParams.push("price=" + this.state.totalPrice)
     const queryString = queryParams.join("&");
     this.props.history.push({
         pathname : '/checkout',
