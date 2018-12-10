a10c0ff4ea898985b3939fcd48eacb7a73badb8e
Gabester0 <gceipper@gmail.com>
2018-12-10 18:44:21 -0500

    Lessons Continued

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,12 @@ class ContactData extends Component {
                     type: "text",
                     placeholder : "Your Name"
                 },
-                value: ""
+                value: "",
+                validation : {
+                    required : true
+                },
+                valid : false,
+                touched : false
             },
             street: {
                 elementType : "input",
@@ -23,7 +28,12 @@ class ContactData extends Component {
                     type: "text",
                     placeholder : "Street"
                 },
-                value: ""
+                value: "",
+                validation : {
+                    required : true
+                },
+                valid : false,
+                touched : false
             },
             country: {
                 elementType : "input",
@@ -31,7 +41,12 @@ class ContactData extends Component {
                     type: "text",
                     placeholder : "Country"
                 },
-                value: ""
+                value: "",
+                validation : {
+                    required : true
+                },
+                valid : false,
+                touched : false
             },
             zip: {
                 elementType : "input",
@@ -39,7 +54,14 @@ class ContactData extends Component {
                     type: "text",
                     placeholder : "ZIP Code"
                 },
-                value: ""
+                value: "",
+                validation : {
+                    required : true,
+                    minLength : 5,
+                    maxLength : 5
+                },
+                valid : false,
+                touched : false
             },
             email: {
                 elementType : "input",
@@ -47,7 +69,12 @@ class ContactData extends Component {
                     type: "email",
                     placeholder : "Email"
                 },
-                value: ""
+                value: "",
+                validation : {
+                    required : true
+                },
+                valid : false,
+                touched : false
             },
             delivery: {
                 elementType : "select",
@@ -57,29 +84,49 @@ class ContactData extends Component {
                         {value : "cheapest", displayValue : "Cheapest"}
                     ]
                 },
-                value: ""
+                value: "",
+                validation : {
+                    required : true
+                },
+                valid: true
             }
         },
         loading: false
     }
 
-orderHandler = (event) => {
+    orderHandler = (event) => {
         event.preventDefault();
-        console.log(this.props.ingredients);
-     this.setState( { loading: true } );
-     const order = {
-         ingredients: this.props.ingredients,
-         price: this.props.price
-     }
-     axios.post('/orders.json', order)
-     .then(response => { this.setState( { loading: false} );
-     this.props.history.push('/');
-     } )
-     .catch(err => {
-         console.log(err);
-         this.setState( { loading: false} );
-     });
+        this.setState( { loading: true } );
+        const formData = {};
+        for(let formElementIdentifier in this.state.orderForm){
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            orderData : formData
+        }
+        axios.post('/orders.json', order)
+        .then(response => { this.setState( { loading: false} );
+        this.props.history.push('/');
+        } )
+        .catch(err => {
+            console.log(err);
+            this.setState( { loading: false} );
+        });
     }
+
+    checkValidity(value, rules){
+        let isValid = false;
+
+        if(rules.required){
+            isValid = value.trim() !== "";
+        }
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && value.length <= rules.maxLength; 
+        }
+        return isValid;
+    };
 
     inputChangedHandler = (event, inputIdentifier) =>{
         const updatedOrderForm = {
@@ -89,7 +136,10 @@ orderHandler = (event) => {
             ...updatedOrderForm[inputIdentifier]
         };
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        updatedFormElement.touched = true;
+        console.log(updatedFormElement.valid);
         this.setState({orderForm : updatedOrderForm});
     }
 
@@ -102,14 +152,18 @@ orderHandler = (event) => {
             });
         }
 
-        let form = ( <form>
+        let form = ( <form onSubmit={this.orderHandler}>
             {formElementsArray.map(formElement => (
                 <Input
-                key={formElement.id}
-                elementType={formElement.config.elementType}
-                elementConfig={formElement.config.elementConfig}
-                value={formElement.config.value}
-                changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
+                    key={formElement.id}
+                    elementType={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    invalid={!formElement.config.valid}
+                    shouldValidate={formElement.config.validation}
+                    touched={formElement.config.touched}
+                    changed={(event) => this.inputChangedHandler(event, formElement.id)}
+                />
             )
             )}
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
